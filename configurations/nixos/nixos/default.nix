34a40cc248e170542e035934b907fdb2b32a2ba9
Vladislav Ekushev <tarnatovski@gmail.com>
2025-11-21 06:04:15 +0200

    Update vscode settings and add opentabletdriver (wip)

--- configurations/nixos/nixos/default.nix
+++ configurations/nixos/nixos/default.nix
@@ -19,6 +19,10 @@ in
     heroic.enable = true;
   };
 
+  hardware.opentabletdriver.enable = true;
+  hardware.uinput.enable = true;
+  boot.kernelModules = [ "uinput" ];
+
   # Workaround the annoying `Failed to start Network Manager Wait Online` error on switch.
   # https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = false;
