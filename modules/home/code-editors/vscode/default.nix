6ecb3c40351b8f80b0548b21ee0bdc9839f30ff9
Vladislav Ekushev <tarnatovski@gmail.com>
2025-11-21 05:00:52 +0200

    Add vscode extensions

--- modules/home/code-editors/vscode/default.nix
+++ modules/home/code-editors/vscode/default.nix
@@ -15,50 +15,50 @@
       enable = true;
       package = pkgs.vscode;
 
-      # profiles.default.extensions = with pkgs.vscode-marketplace; [
-      #   # Rust
-      #   rust-lang.rust-analyzer
+      profiles.default.extensions = with pkgs.vscode-marketplace; [
+        # Rust
+        rust-lang.rust-analyzer
 
-      #   # Svelte
-      #   svelte.svelte-vscode
+        # Svelte
+        svelte.svelte-vscode
 
-      #   # Nix
-      #   bbenoist.nix
-      #   jnoortheen.nix-ide
+        # Nix
+        bbenoist.nix
+        jnoortheen.nix-ide
 
-      #   # File formats
-      #   redhat.vscode-yaml
-      #   tamasfe.even-better-toml
-      #   jock.svg
-      #   shanoor.vscode-nginx
-      #   tomoki1207.pdf
-      #   davidanson.vscode-markdownlint
-      #   yzane.markdown-pdf
+        # File formats
+        redhat.vscode-yaml
+        tamasfe.even-better-toml
+        jock.svg
+        shanoor.vscode-nginx
+        tomoki1207.pdf
+        davidanson.vscode-markdownlint
+        yzane.markdown-pdf
 
-      #   # Tools
-      #   dbaeumer.vscode-eslint
-      #   esbenp.prettier-vscode
-      #   ms-azuretools.vscode-docker
-      #   ms-vscode.makefile-tools
-      #   eamodio.gitlens
-      #   mhutchie.git-graph
-      #   adpyke.codesnap
-      #   ms-vscode.hexeditor
-      #   ms-vsliveshare.vsliveshare
-      #   ms-vscode-remote.remote-containers
-      #   ms-vscode-remote.remote-ssh
-      #   ms-vscode-remote.remote-ssh-edit
-      #   ms-vscode-remote.remote-wsl
-      #   ms-vscode.remote-explorer
-      #   ms-playwright.playwright
+        # Tools
+        dbaeumer.vscode-eslint
+        esbenp.prettier-vscode
+        ms-azuretools.vscode-docker
+        ms-vscode.makefile-tools
+        eamodio.gitlens
+        mhutchie.git-graph
+        adpyke.codesnap
+        ms-vscode.hexeditor
+        ms-vsliveshare.vsliveshare
+        ms-vscode-remote.remote-containers
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        ms-vscode-remote.remote-wsl
+        ms-vscode.remote-explorer
+        ms-playwright.playwright
 
-      #   # UI
-      #   pkief.material-icon-theme
-      #   wix.vscode-import-cost
-      #   yoavbls.pretty-ts-errors
-      # ] ++ (with pkgs.vscode-extensions; [
-      #   vadimcn.vscode-lldb
-      # ]);
+        # UI
+        pkief.material-icon-theme
+        wix.vscode-import-cost
+        yoavbls.pretty-ts-errors
+      ] ++ (with pkgs.vscode-extensions; [
+        vadimcn.vscode-lldb
+      ]);
     };
 
     home.file.".config/Code/User/settings.json".source = lib.mkDefault ./settings.json;
